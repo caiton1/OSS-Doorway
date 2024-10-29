633fce45932948ed4632f3f54678efb4e0db02fe
Jadyn Calhoun <jlc2243@nau.edu>
2024-10-28 17:05:52 -0700

    updates to hint schema

--- src/models/HintModel.js
+++ src/models/HintModel.js
@@ -7,6 +7,14 @@ const mongoose = require("mongoose")
 //! hints have video, image, and text attributes and those can be customized
 //!hints are split to sections: quest completion, current progress, hints used, etc
 const HintSchema = mongoose.Schema({
+    quest:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Quest"
+    },
+    task:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Task"
+    },
     sequence:{
         type: Number,
         required: [true, "Please provide a sequence for the hint"]
